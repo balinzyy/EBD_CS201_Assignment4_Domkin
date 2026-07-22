d6ddd7f1a9063bd32278367b58b2bd0bb0b00732
pt2 <petro.domkin.25@kse.org.ua>
2026-07-22 23:26:12 +0300

    3 task (data clearing)

--- assignment 4.py
+++ assignment 4.py
@@ -5,6 +5,36 @@ with open('global_sales.csv', "r", encoding="UTF-8") as file:
     global_sales = list(csv.DictReader(file))
     print(global_sales)
 
+
 with open("regional_tariffs.json", "r", encoding="UTF-8") as file:
     regional_tariffs = json.load(file)
     print(regional_tariffs)
+
+for row in global_sales:
+    for column in ['quantity', 'revenue']:
+        if row.get(column) == "N/A" or not row.get(column):
+            row[column] = 0.0
+        else:
+            row[column] = float(row[column])
+
+print(global_sales)
+
+
+for region, tarrif in regional_tariffs.items():
+    if tarrif == 'N/A':
+        regional_tariffs[region] = 0.0
+    else:
+        regional_tariffs[region] = float(tarrif)
+
+print(regional_tariffs)
+
+
+
+
+
+for i in global_sales:
+    i["net_profit"] = i["revenue"] - i["revenue"] * (regional_tariffs[i["region"]]/100)
+
+
+
+
